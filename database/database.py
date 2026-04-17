f41d1aeb18e8fc6f12fa38faee3d97c613c6f374
Peter Osas <osaspeter6@gmail.com>
2026-04-17 11:19:35 +0100

    move db to supabase

--- database/database.py
+++ database/database.py
@@ -1,16 +1,14 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///stage_one.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.abjkfhxzgdbjxiqxrlof:2ZZWdV3KVgcbAq42@aws-1-eu-west-1.pooler.supabase.com:6543/postgres"
 
-engine = create_engine(
-     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind= engine)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
